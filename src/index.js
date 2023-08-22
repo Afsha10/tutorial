f364ef2a1e9b4824c77ca5b1d5f7d18280092371
Afsha10 <unityofbeing1@gmail.com>
2023-08-22 16:06:29 +0100

    Props - Somewhat Dynamic Setup
    - Created two different objects with different values inside firstBook and secondBook by refactoring variables to properties
    - Then we set up props a bit more dynamically eg. firstBook.title
    - We pass the values inside the objects through the props to our books components
    - We get a somewhat dynamic list and we are moving toward a more dynamic direction
    - The structure of the book component is the same, but since we are providing different values or data to our props, the result we see in our screen is also different.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const title = "Iron Flame";
-const author = "Rebecca Yarros";
-const img = "./images/book-1.jpg";
+const firstBook = {
+  author: "Rebecca Yarros",
+  title: "Iron Flame",
+  img: "./images/book-1.jpg"
+}
+
+const secondBook = {
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book title={title} author={author} img={img} />
-      <Book title={title} author={author} img={img} />
+      <Book title={firstBook.title} author={firstBook.author} img={firstBook.img} />
+      <Book title={secondBook.title} author={secondBook.author} img={secondBook.img} />
     </section>
   );
 };
