9f6293e9e1689821e627332b2eff21a7675f0f8e
Afsha10 <unityofbeing1@gmail.com>
2023-08-22 20:46:26 +0100

    Proper List
    We create an array of objects called books.
    
    In react we cannot render or display an object directly like below in JSX:
    
    const BookList = () => {
      return <section className="booklist">{books}</section>;
    };
    
    Map: A map method creates a new array from calling a function for every array element.
    
    The solution is to use map method to iterate over the books array data and for every item we will return a component. We pull the properties out of the object, and pass them one by one to the book component. Thus, for every item, we return this component: <Book title= {title} img={img} author={author} />.
    
    And we get this awesome setup.
    
    All our logic is inside the Book component to render stuff on the screen. We simply need to iterate over and for every item we just need to return the component.
    
    We are iterating over the list and we are wrapping the elements in some kind of html structure.

--- src/index.js
+++ src/index.js
@@ -2,50 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  author: "Rebecca Yarros",
-  title: "Iron Flame",
-  img: "./images/book-1.jpg",
-};
-
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
-};
+const books = [
+  {
+    author: "Rebecca Yarros",
+    title: "Iron Flame",
+    img: "./images/book-1.jpg",
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          sit at modi asperiores tenetur, ut porro? Laboriosam natus eveniet
-          illum quisquam iure, dignissimos repellat a quidem aliquam commodi.
-          Doloribus, maiores?
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const {img, title, author} = book;
+        return (
+          <div>
+            <Book title= {title} img={img} author={author} />
+          </div>
+        );
+      })}
     </section>
   );
 };
 
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
