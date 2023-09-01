0f8d5fafcfdb46f9e3a82250de4d06a816556cc3
Afsha10 <unityofbeing1@gmail.com>
2023-09-01 23:26:45 +0100

    Complex Example- Fix 2
    We still pass the reference. We are now naming the function here and we cannot invoke the function right away. We get the ID that belongs to that component as we are iterating over the list.

--- src/index.js
+++ src/index.js
@@ -34,11 +34,14 @@ const BookList = () => {
 
 const Book = (props) => {
   const { img, title, author, id, getBook } = props;
+  const getSingleBook = () => {
+    getBook(id);
+  }
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>Click me</button>
+      <button onClick={getSingleBook}>Click me</button>
       <h4>{author}</h4>
     </article>
   );
