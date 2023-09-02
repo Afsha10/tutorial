5fb5ebd73a776a634a223c92fd1f9905d64d0e9a
Afsha10 <unityofbeing1@gmail.com>
2023-09-02 23:59:20 +0100

    ES6 modules - export/import with the named one
    We split up our application into multiple files, folders and the ability to import and export the code which helps us to organise our application. As our  application grows it is not sustainable to have such big index.js file. So we split up the code where we have separate functionalities in different places. If we need the component or data, we just access it. We use ES6 modules to do that. This is not unique to React.
    
    When it comes to export and import, we have 2 flavours- a named one and a default one. If we have one component in the file,  we do that as a default one. If we have multiples, they will be named.
    
    When we import with a named one, we need to use the same name. With the default one, we can come up with whatever we want.
    
    So in the src we set up two files- books.js forour data and Book.js for our component. We remove books array from index.js and add it to Books.js
    
    When we import books.js inside index.js we use curlies which means we will get specific item i.e. the item we are exporting is exported as named export.  Sp {book} needs to match what is exactly in the file.

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: "Rebecca Yarros",
+    title: "Iron Flame",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,51 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
-const books = [
-  {
-    author: "Rebecca Yarros",
-    title: "Iron Flame",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg",
-    id: 2,
-  },
-];
+import "./index.css";
+import {books} from './books'
+import Book from './Book';
 
 const BookList = () => {
-  function getBook(id) {
-      const matchingBook = books.find((book) => book.id === id)
-      console.log(matchingBook);
-  }
 
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 };
 
-const Book = (props) => {
-  const { img, title, author, id, getBook } = props;
-  const getSingleBook = () => {
-    getBook(id);
-  }
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>Click me</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
